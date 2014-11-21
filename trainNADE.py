1ca06da15ad8373441a289b48d044f6d7c186bb3
Mathieu Germain <mathieu.germain2@usherbrooke.ca>
2014-11-21 16:01:38 -0500

    Added early stopping to training

--- trainNADE.py
+++ trainNADE.py
@@ -13,13 +13,19 @@ def get_done_text(start_time):
     return "DONE in {:.4f} seconds.".format(t.time() - start_time)
 
 
-def trainNADE(save_path, batch_size=100, hidden_size=500, learning_rate=0.05, max_epochs=100, tied=False):
+def trainNADE(save_path, batch_size=100, hidden_size=500, learning_rate=0.05, max_epochs=100, look_ahead=15, tied=False):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     print "## Loading dataset ...",
     start_time = t.time()
     dataset = np.load('binarized_mnist.npz')['arr_0'].item()
+
+    ratio = 5000
+    for sub in ['train', 'valid', 'test']:
+        dataset[sub]['length'] = dataset[sub]['length'] / ratio
+        dataset[sub]['data'] = dataset[sub]['data'][:int(dataset[sub]['length'])]
+
     print get_done_text(start_time)
 
     index = T.lscalar()
@@ -49,16 +55,23 @@ def trainNADE(save_path, batch_size=100, hidden_size=500, learning_rate=0.05, ma
     #theano.printing.pydotprint(train_model, 'train_model')
     print get_done_text(start_time)
 
-    print "## Training batch={0} hidden_size={1} ##".format(batch_size, hidden_size)
+    print "## Training NADE: batch={0} hidden_size={1} tied={2} ##".format(batch_size, hidden_size, tied)
     start_time_train = t.time()
-    best_epoch = 0
-    best_nll = np.inf
-    for epoch in xrange(max_epochs):
-        print "Epoch ", epoch
 
-        nb_iterations = int(np.ceil(dataset['train']['length'] / batch_size))
+    trainer_status = {
+        "best_valid_error": np.inf,
+        "best_epoch": 0,
+        "epoch": 0,
+        "nb_of_epocs_without_improvement": 0
+    }
+
+    while(trainer_status["epoch"] < max_epochs and trainer_status["nb_of_epocs_without_improvement"] < look_ahead):
+        trainer_status["epoch"] += 1
+
+        print "Epoch ", trainer_status["epoch"]
         print '\tTraining   ...',
         train_err = 0
+        nb_iterations = int(np.ceil(dataset['train']['length'] / batch_size))
         start_time = t.time()
         for index in range(nb_iterations):
             train_err += train_model(index)
@@ -72,14 +85,17 @@ def trainNADE(save_path, batch_size=100, hidden_size=500, learning_rate=0.05, ma
             valid_err += valid_nll(index)
         print get_done_text(start_time), " NLL: {0:.6f}".format(valid_err / nb_iterations_valid)
 
-        if valid_err < best_nll:
-            best_epoch = epoch
-            best_nll = best_nll
+        if valid_err < trainer_status["best_valid_error"]:
+            trainer_status["best_valid_error"] = valid_err
+            trainer_status["best_epoch"] = trainer_status["epoch"]
+            trainer_status["nb_of_epocs_without_improvement"] = 0
             model.save(save_path)
+        else:
+            trainer_status["nb_of_epocs_without_improvement"] += 1
 
     print "### Training", get_done_text(start_time_train), "###"
 
-    print '\n### Evaluating best model from Epoch {0} ###'.format(best_epoch)
+    print '\n### Evaluating best model from Epoch {0} ###'.format(trainer_status["best_epoch"])
     model.load(save_path)
     for subset in ['test', 'valid', 'train']:
         nll = locals()["{}_nll".format(subset)]
@@ -89,4 +105,6 @@ def trainNADE(save_path, batch_size=100, hidden_size=500, learning_rate=0.05, ma
             err += nll(index)
         print "\tBest {1} error is : {0:.6f}".format(err / nb_iterations, subset)
 
-trainNADE("result/bmnist/", batch_size=100, hidden_size=500, learning_rate=0.05, max_epochs=100, tied=True)
+
+if __name__ == '__main__':
+    trainNADE("result/bmnist/", batch_size=100, hidden_size=500, learning_rate=0.05, max_epochs=np.inf, tied=True)
