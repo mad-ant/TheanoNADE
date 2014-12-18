53cfd2ad2a8cd63cfb93fe364d3f59b3c3ad0514
Mathieu Germain <mathieu.germain@gmail.com>
2014-12-18 14:47:03 -0500

    modified utils

--- utils.py
+++ utils.py
@@ -4,7 +4,25 @@ import pickle
 import json
 import theano
 import hashlib
-from GoogleSheetAPI import GSheet
+import fcntl
+#from GoogleSheetAPI import GSheet#
+import os
+from contextlib import contextmanager
+import logging
+
+
+@contextmanager
+def open_with_lock(*args, **kwargs):
+    """ Context manager for opening file with an exclusive lock. """
+    f = open(*args, **kwargs)
+    try:
+        fcntl.flock(f.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError:
+        logging.info("Can't immediately write-lock the file ({0}), blocking ...".format(f.name))
+        fcntl.flock(f.fileno(), fcntl.LOCK_EX)
+    yield f
+    fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+    f.close()
 
 
 def generate_uid_from_string(value):
@@ -23,38 +41,42 @@ def saveFeatures(model):
     #     features = np.c_[features, w]
     pickle.dump(features, open('W.pkl', 'wb'))
 
-# def write_result(dataset_name, model_info):
-#     result_file = "_RESULTS_{0}_deep-nadeTheano.csv".format(dataset_name)
-#     write_header = not os.path.exists(result_file)
-#     with open(result_file, "a") as f:
-#         if write_header:
-#             f.write("Learning Rate\tDecrease Constant\tHidden Layers\tRandom Seed\tActivation Function\tMax Epoch\tBest Epoch\tLook Ahead\tBatch Size\tShuffle Mask\tNb Shuffle Per Valid\tConditioning Mask\tDirect Input Connect\tDirect Output Connect\tPre-Training\tPre-Training Max Epoch\tMomentum\tDropout Rate\tWeights Initialization\tTraining err\tTraining err std\tValidation err\tValidation err std\tTest err\tTest err std\tTotal Training Time\n")
-#         f.write('\t'.join(map(str, model_info[0:19])) + "\t" + '\t'.join(map("{0:.6f}".format, model_info[19:-1])) + "\t" + "{0:.4f}\n".format(model_info[-1]))
-
 
 def write_result(dataset_name, model_info, experiment_name):
-    print "### Savin result to the cloud ^^ ...",
-    start_time = t.time()
+    result_file = "_RESULTS_{0}_nadeTheano.csv".format(dataset_name)
+    write_header = not os.path.exists(result_file)
 
     header = ["Learning Rate", "Decrease Constant", "Hidden Layers", "Random Seed", "Activation Function", "Tied Weights", "Max Epoch", "Best Epoch", "Look Ahead", "Batch Size", "Momentum", "Dropout Rate", "Weights Initialization", "Training err", "Training err std", "Validation err", "Validation err std", "Test err", "Test err std", "Total Training Time", "Experiment Name"]
 
-    sheet = GSheet('1gwMuCMHE0mhu7EMP74djEYARfSvnBrMnsAE-cTGJS2w', 'mathieu.germain@gmail.com', 'nigvtrjsrshqzsqb')
-    worksheetID = sheet.getWorksheetID(dataset_name)
+    with open_with_lock(result_file, "a") as f:
+        if write_header:
+            f.write("\t".join(header) + "\n")
+        f.write('\t'.join(map(str, model_info[0:13])) + "\t" + '\t'.join(map("{0:.6f}".format, model_info[13:-1])) + "\t" + "{0:.4f}".format(model_info[-1]) + "\t{0}\n".format(experiment_name))
 
-    if worksheetID is None:
-        worksheetID = sheet.createWorksheet(dataset_name, header)
 
-    result = model_info[0:21]
-    result += map("{0:.6f}".format, model_info[21:-1])
-    result += ["{0:.4f}".format(model_info[-1])]
-    result += [experiment_name]
+# def write_result(dataset_name, model_info, experiment_name):
+# print "### Savin result to the cloud ^^ ...",
+#     start_time = t.time()
 
-    try:
-        sheet.addRow(worksheetID, result)
-    except:
-        sheet.addRow(worksheetID, result)
+#     header = ["Learning Rate", "Decrease Constant", "Hidden Layers", "Random Seed", "Activation Function", "Tied Weights", "Max Epoch", "Best Epoch", "Look Ahead", "Batch Size", "Momentum", "Dropout Rate", "Weights Initialization", "Training err", "Training err std", "Validation err", "Validation err std", "Test err", "Test err std", "Total Training Time", "Experiment Name"]
 
-    print get_done_text(start_time)
+#     sheet = GSheet('1gwMuCMHE0mhu7EMP74djEYARfSvnBrMnsAE-cTGJS2w', 'mathieu.germain@gmail.com', 'nigvtrjsrshqzsqb')
+#     worksheetID = sheet.getWorksheetID(dataset_name)
+
+#     if worksheetID is None:
+#         worksheetID = sheet.createWorksheet(dataset_name, header)
+
+#     result = model_info[0:21]
+#     result += map("{0:.6f}".format, model_info[21:-1])
+#     result += ["{0:.4f}".format(model_info[-1])]
+#     result += [experiment_name]
+
+#     try:
+#         sheet.addRow(worksheetID, result)
+#     except:
+#         sheet.addRow(worksheetID, result)
+
+#     print get_done_text(start_time)
 
 
 def get_done_text(start_time):
