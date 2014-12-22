eb2a10a7e269d7a68e51b48cc1410ace6aeb4515
Mathieu Germain <mathieu.germain2@usherbrooke.ca>
2014-12-22 16:41:03 -0500

    Cleaned the save result mechanism

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import json
 import theano
 import hashlib
 import fcntl
-#from GoogleSheetAPI import GSheet#
+from GoogleSheetAPI import GSheet
 import os
 from contextlib import contextmanager
 import logging
@@ -43,40 +43,48 @@ def saveFeatures(model):
 
 
 def write_result(dataset_name, model_info, experiment_name):
-    result_file = "_RESULTS_{0}_nadeTheano.csv".format(dataset_name)
-    write_header = not os.path.exists(result_file)
-
     header = ["Learning Rate", "Decrease Constant", "Hidden Layers", "Random Seed", "Activation Function", "Tied Weights", "Max Epoch", "Best Epoch", "Look Ahead", "Batch Size", "Momentum", "Dropout Rate", "Weights Initialization", "Training err", "Training err std", "Validation err", "Validation err std", "Test err", "Test err std", "Total Training Time", "Experiment Name"]
+
+    result = map(str, model_info[0:13])
+    result += map("{0:.6f}".format, model_info[13:-1])
+    result += ["{0:.4f}".format(model_info[-1])]
+    result += [experiment_name]
+
+    try:
+        print "### Savin result to the cloud ^^ ...",
+        start_time = t.time()
+        write_result_gsheet(dataset_name, header, result)
+    except:
+        print "FAIL!"
+        print "### Savin result to file instead ...",
+        start_time = t.time()
+        write_result_file(dataset_name, header, result)
+
+    print get_done_text(start_time)
+
+
+def write_result_file(dataset_name, header, result):
+    result_file = "_RESULTS_{0}_nadeTheano.csv".format(dataset_name)
+
+    write_header = not os.path.exists(result_file)
 
     with open_with_lock(result_file, "a") as f:
         if write_header:
             f.write("\t".join(header) + "\n")
-        f.write('\t'.join(map(str, model_info[0:13])) + "\t" + '\t'.join(map("{0:.6f}".format, model_info[13:-1])) + "\t" + "{0:.4f}".format(model_info[-1]) + "\t{0}\n".format(experiment_name))
+        f.write('\t'.join(result) + "\n")
 
 
-# def write_result(dataset_name, model_info, experiment_name):
-# print "### Savin result to the cloud ^^ ...",
-#     start_time = t.time()
+def write_result_gsheet(dataset_name, header, result):
+    sheet = GSheet('1gwMuCMHE0mhu7EMP74djEYARfSvnBrMnsAE-cTGJS2w', 'mathieu.germain@gmail.com', 'nigvtrjsrshqzsqb')
+    worksheetID = sheet.getWorksheetID(dataset_name)
 
-#     header = ["Learning Rate", "Decrease Constant", "Hidden Layers", "Random Seed", "Activation Function", "Tied Weights", "Max Epoch", "Best Epoch", "Look Ahead", "Batch Size", "Momentum", "Dropout Rate", "Weights Initialization", "Training err", "Training err std", "Validation err", "Validation err std", "Test err", "Test err std", "Total Training Time", "Experiment Name"]
+    if worksheetID is None:
+        worksheetID = sheet.createWorksheet(dataset_name, header)
 
-#     sheet = GSheet('1gwMuCMHE0mhu7EMP74djEYARfSvnBrMnsAE-cTGJS2w', 'mathieu.germain@gmail.com', 'nigvtrjsrshqzsqb')
-#     worksheetID = sheet.getWorksheetID(dataset_name)
-
-#     if worksheetID is None:
-#         worksheetID = sheet.createWorksheet(dataset_name, header)
-
-#     result = model_info[0:21]
-#     result += map("{0:.6f}".format, model_info[21:-1])
-#     result += ["{0:.4f}".format(model_info[-1])]
-#     result += [experiment_name]
-
-#     try:
-#         sheet.addRow(worksheetID, result)
-#     except:
-#         sheet.addRow(worksheetID, result)
-
-#     print get_done_text(start_time)
+    try:
+        sheet.addRow(worksheetID, result)
+    except:
+        sheet.addRow(worksheetID, result)
 
 
 def get_done_text(start_time):
